493726fd6ab08b3693ecfd832cdf65e367771ab0
Michelle Li <limichelle21@gmail.com>
2017-05-22 08:17:14 -0700

    error handling for no units params

--- README.md
+++ README.md
@@ -8,8 +8,7 @@ Rails 5.1.1
 
 This web service takes a string of units multiplied and/or divided, and converts these units into their corresponding SI units along with a multiplication factor that can be used to convert the original units into the SI units. 
 
-The service will return a JSON object. 
-
+The service will return a JSON object.
 
 Input units and their corresponding SI units have been seeded into the database. 
 
--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,61 +1,44 @@
 class UnitsController < ApplicationController
     
-  def si
+    def si
       units = params[:units]
-   
-# retrieve all units inside a string
-          
-      inputs = units.scan(/\w+|"|'|°/)
 
-      
-#      inputs = units.split("/")
-#      raise inputs.inspect
-     
-# find the corresponding SI unit for each unit in string, store in array
-# si is an array of stdunit objects
-      
-      si = inputs.map do |x|
-          puts "><><><><><> #{x.inspect}"
-          Unit.find_by(name: x).stdunit
-      end  
-      
-# copy of original string for units
-      
-      unit_string = units.dup 
-      
-      
-# copy of original string for factors
-      
-      factor_string = units.dup 
-      
-      
-# replace all words in original string with si unit names
-      
-      inputs.each.with_index do |c, i|
-          unit_string.scan(c).size.times do
-              unit_string.gsub!(c, si[i].name)
+      if units.present?
+
+          inputs = units.scan(/\w+|"|'|°/)
+
+
+          si = inputs.map do |x|
+              puts "><><><><><> #{x.inspect}"
+              Unit.find_by(name: x).stdunit
+          end  
+
+          unit_string = units.dup 
+          factor_string = units.dup 
+
+
+          inputs.each.with_index do |c, i|
+              unit_string.scan(c).size.times do
+                  unit_string.gsub!(c, si[i].name)
+              end
           end
+
+          inputs.each.with_index do |c, i|
+              factor_string.scan(c).size.times do
+                  factor_string.gsub!(c, "#{si[i].factor}")
+              end
+          end
+
+          m_factor = eval(factor_string).round(14)
+
+          render json: { unit_name: unit_string, multiplication_factor: m_factor }
+
+      else
+
+          render json: { status: 400, error: "Please enter in valid units" }
       end
 
-      
-      
-# replace all words in original string with si unit factors
-      
-      inputs.each.with_index do |c, i|
-          factor_string.scan(c).size.times do
-              factor_string.gsub!(c, "#{si[i].factor}")
-          end
-      end
-   
-
-      
-# MATH - evaluate the factors in original string format
-      
-      m_factor = eval(factor_string).round(14)
-      
-      render json: { unit_name: unit_string, multiplication_factor: m_factor }
-            
-  end
+    end
 
     
 
